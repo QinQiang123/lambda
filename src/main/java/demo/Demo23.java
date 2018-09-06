208b80d9753009e4f635a5d04b630b609d0f242e
‘chrish-2017’ <932971733@qq.com>
2018-09-06 17:12:54 +0800

    按名称将商品进行分组，统计相同名称商品的数量

---
+++ src/main/java/demo/Demo23.java
@@ -0,0 +1,32 @@
+package demo;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author QinQiang
+ * @since 2018-09-06 14:46
+ */
+public class Demo23 {
+    public static void main(String[] args) {
+
+        Map<String, Object> map = new HashMap<>(16);
+
+
+        map.put("num1", 1);
+        map.put("num2", 2);
+        map.put("num3", 3);
+        map.put("num4", 4);
+        map.put("num5", 5);
+        map.put("num6", 6);
+
+        map.forEach((key, value) -> {
+            if ("num2".equals(key)) {
+                System.out.println(value);
+            }
+           /* System.out.println(key);
+            System.out.println(value);*/
+        });
+
+    }
+}
