392623326a9658520e3ebec85a4d217e78dd1add
Mloong <164481121@qq.com>
2018-09-14 17:53:59 +0800

    测试冲突

--- src/main/java/demo/Demo1.java
+++ src/main/java/demo/Demo1.java
@@ -28,6 +28,9 @@ public class Demo1 {
         System.out.println(hiddenFiles2);
         System.out.println(111111);
         System.out.println(hiddenFiles2);
+        System.out.println(hiddenFiles2);
+        System.out.println(hiddenFiles2);
+        System.out.println(hiddenFiles2);
 
 
     }
